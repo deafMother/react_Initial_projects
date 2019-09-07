9d95a6e667dcac9139e6a7bbfdd0c3c314d691ba
Sarovar <iamsarovarjs@gmail.com>
2019-09-07 22:37:37 +0530

    another commit

--- context-demo1/src/context/LanguageContext.js
+++ context-demo1/src/context/LanguageContext.js
@@ -1,5 +1,5 @@
 // create a context object
 
-import React from "react";
-// now the default value os set to 'english'.
-export default React.createContext("english");
+import React from 'react';
+// now the default value is set to 'english'.
+export default React.createContext('english');
